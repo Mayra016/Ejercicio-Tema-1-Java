caefaf1114fd29e3f28d309771c81c36b809f185
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2023-05-02 16:07:44 -0300

    Create Tema1.java

---
+++ Tema1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ 
+public class Tema1 {
+    public static void main(String[] args) throws Exception {        
+            int num1 = 1000;
+            long num2 = 1000000;
+            double num3 = 30.04567d;
+            boolean button = true;
+            String texto = "Ejercicio de datos primitivos en Java";
+            System.out.println(texto);
+            System.out.println(num1);
+            System.out.println(num2);
+            System.out.println(num3);
+            System.out.println(button);
+    }
+    
+}
